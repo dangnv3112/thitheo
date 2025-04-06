b9047ee59246e3e5d5a4db0c8a4de5266e49541a
Your Name <your.email@example.com>
2025-04-06 20:42:47 +0700

    Update next.config.js to ignore ESLint errors during build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: { unoptimized: true },
   basePath: '/thitheo',
   trailingSlash: true,
+  eslint: {
+    // Bỏ qua lỗi ESLint trong quá trình build
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
