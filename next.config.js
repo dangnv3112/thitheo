1b60d84acda218adf814dd4337c1046c053b3da6
dangnv3112 <120073906+dangnv3112@users.noreply.github.com>
2025-04-06 20:30:44 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/thitheo',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
