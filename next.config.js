5930758136a8c43a4878a759baba17520931a606
Your Name <your.email@example.com>
2025-04-06 20:48:57 +0700

    Add exportPathMap to handle dynamic routes in static export

--- next.config.js
+++ next.config.js
@@ -12,6 +12,29 @@ const nextConfig = {
     // Bỏ qua lỗi TypeScript trong quá trình build
     ignoreBuildErrors: true,
   },
+  // Bỏ qua các lỗi Dynamic Routes khi export static
+  experimental: {
+    missingSuspenseWithCSRInDevelopment: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      '/cart': { page: '/cart' },
+      '/products': { page: '/products' },
+      '/promotions': { page: '/promotions' },
+      '/search': { page: '/search' },
+      '/user': { page: '/user' },
+      '/products/1': { page: '/products/[id]', query: { id: '1' } },
+      '/products/2': { page: '/products/[id]', query: { id: '2' } },
+      '/products/3': { page: '/products/[id]', query: { id: '3' } },
+      '/products/4': { page: '/products/[id]', query: { id: '4' } },
+    }
+  },
 }
 
 module.exports = nextConfig
